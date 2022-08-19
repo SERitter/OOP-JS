a7f42c571120c6db4c64922d8b95399f1255b36c
Shawn Ritter <83777884+SERitter@users.noreply.github.com>
2022-08-18 22:36:23 -0600

    Pre Value Vs Reference

--- index.js
+++ index.js
@@ -14,28 +14,50 @@
 
 // circle.draw();
 
-//Object Factory
-function createCircle(radius) {
-  return {
-    //ES6 has a feature, where if the key and the value are the same you only need to add it once
-    //radius: radius,
-    radius,
-    draw: function () {
-      console.log('draw');
-    },
-  };
-}
+// //Object Factory
+// function createCircle(radius) {
+//   return {
+//     //ES6 has a feature, where if the key and the value are the same you only need to add it once
+//     //radius: radius,
+//     radius,
+//     draw: function () {
+//       console.log('draw');
+//     },
+//   };
+// }
 
-const circle = createCircle(1);
-circle.draw();
+// // const circle = createCircle(1);
+// // circle.draw();
 
-//Object Constructor
-function Circle(radius) {
-  this.radius = radius;
-  this.draw = function () {
-    console.log('draw');
-  };
-}
+// //Object Constructor
+// function Circle(radius) {
+//   this.radius = radius;
+//   this.draw = function () {
+//     console.log('draw');
+//   };
+// }
 
-const another = new Circle(1);
-another.draw();
+// // const another = new Circle(1);
+// // another.draw();
+
+// // //Functions are Objects
+// // const Circle1 = new Function(
+// //   'radius',
+// //   `
+// //     this.radius = radius;
+// //     this.draw = function () {
+// //       console.log('draw');
+// //     }
+// //   `
+// // );
+
+// // const circle1 = new Circle1(1);
+// // circle1.draw();
+
+// const another = new Circle(1);
+// another.draw();
+// const another2 = Circle.call({}, 1);
+// Circle.call({}, 1);
+// // another2.draw();
+// const another3 = Circle.apply({}, [1]);
+// // another3.draw();
