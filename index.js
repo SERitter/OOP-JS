d757423ae91330c2de45b48090408a52a1f97f8c
Shawn Ritter <83777884+SERitter@users.noreply.github.com>
2022-08-20 13:13:43 -0600

    exercise2 solution

--- index.js
+++ index.js
@@ -177,6 +177,60 @@
   </select>
 */
 
+// function HtmlElement() {
+//   this.click = function () {
+//     console.log('clicked');
+//   };
+// }
+
+// HtmlElement.prototype.focus = function () {
+//   console.log('focused');
+// };
+
+// function HtmlSelectElement(items = []) {
+//   this.items = items;
+
+//   this.addItem = function (value) {
+//     this.items.push(value);
+//   };
+
+//   this.removeItem = function (value) {
+//     this.items.splice(this.items.indexOf(value), 1);
+//   };
+
+//   this.render = function () {
+//     let output = '<select>\n';
+//     for (item of items) {
+//       output += '\t<option>' + item + '</option>\n';
+//     }
+//     output += '</select>';
+//     return output;
+//   };
+// }
+
+// HtmlSelectElement.prototype = new HtmlElement();
+
+// function HtmlImageElement(src) {
+//   this.src = src;
+//   this.render = function () {
+//     return '<img src="' + src + '" />';
+//   };
+// }
+
+// HtmlImageElement.prototype = new HtmlElement();
+
+// const e = new HtmlElement();
+
+// const s = new HtmlSelectElement([1, 2, 3]);
+
+// const i1 = new HtmlImageElement('https://');
+
+// const elements = [new HtmlSelectElement([1, 2, 3]), new HtmlImageElement('https://')];
+
+// for (element of elements) console.log(element.render());
+
+// Exercise 2 solution
+
 function HtmlElement() {
   this.click = function () {
     console.log('clicked');
@@ -199,12 +253,23 @@ function HtmlSelectElement(items = []) {
   };
 
   this.render = function () {
-    let output = '<select>\n';
-    for (item of items) {
-      output += '\t<option>' + item + '</option>\n';
-    }
-    output += '</select>';
-    return output;
+    // let output = '<select>\n';
+    // for (item of items) {
+    //   output += '\t<option>' + item + '</option>\n';
+    // }
+    // output += '</select>';
+    // return output;
+    return `
+    <select>
+      ${this.items
+        .map(
+          (item) => `
+        <option>${item}</option>
+      `
+        )
+        .join('')}
+    </select>
+    `;
   };
 }
 
@@ -212,12 +277,14 @@ HtmlSelectElement.prototype = new HtmlElement();
 
 function HtmlImageElement(src) {
   this.src = src;
+
   this.render = function () {
-    return '<img src="' + src + '" />';
+    return `<img src="${this.src}" />`;
   };
 }
 
 HtmlImageElement.prototype = new HtmlElement();
+HtmlImageElement.prototype.constructor = HtmlImageElement;
 
 const e = new HtmlElement();
 
